b3dc13f0a10abe8eb986072b7e9459c5f548c852
Futtock123 <68926656+Futtock123@users.noreply.github.com>
2022-10-07 11:51:33 -0400

    Update client.c

--- client.c
+++ client.c
@@ -9,13 +9,13 @@ int main(int argc, char* argv[])
     char serverName[16];
     int serverPort = 0;
 
-    printf("Enter server IP address: /n");
+    printf("Enter server IP address: \n");
     scanf("%s", serverName);
-    printf("/n");
+    printf("\n");
 
-    printf("Enter server port number: /n");
+    printf("Enter server port number: \n");
     scanf("%d", &serverPort);
-    printf("/n");
+    printf("\n");
 
     printf("The IP address you entered is %s.", serverName);
     printf("The port number you entered is %d.", serverPort);
