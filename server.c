89491ab6779599d695d79344242ef64532afc8d1
John Winward <john.winward2@gmail.com>
2022-10-07 11:27:13 -0400

    add port number entry through args

---
+++ server.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[]){
+    int port;
+
+    if(argc == 2){
+        port = argv[1];
+        //Debug
+        printf("Port entered: %d", port);
+    }
+    else{
+        printf("Invalid # of arguments");
+        return 1;
+    }
+
+    return 0;
+}
