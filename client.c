d93691655c0086ab72956c9916e70407152cf089
Futtock123 <68926656+Futtock123@users.noreply.github.com>
2022-10-15 14:29:21 -0400

    Update client.c

--- client.c
+++ client.c
@@ -4,30 +4,38 @@
 #include <stdio.h>
 
 
-int main(int argc, char const* argv[])
+int main(int argc, char* argv[])
 {
-    char serverName[16];
     char* clntMes = "This is the client! Hello server!";
     int serverPort = 0;
+    struct sockaddr_in serverName;
     int s = 0;
 
-    printf("Enter server IP address: \n");
-    scanf("%s", serverName);
-    printf("\n");
+    //Gather and display user input
+    // printf("Enter server IP address: \n");
+    // scanf("%s", serverName);
+    // printf("\n");
 
-    printf("Enter server port number: \n");
-    scanf("%d", &serverPort);
-    printf("\n");
+    // printf("Enter server port number: \n");
+    // scanf("%d", &serverPort);
+    // printf("\n");
 
-    printf("The IP address you entered is %s.", serverName);
-    printf("The port number you entered is %d.", serverPort);
-    printf("\n")
+    // printf("The IP address you entered is %s.", serverName);
+    // printf("The port number you entered is %d.", serverPort);
+    // printf("\n")
 
+    //Create socket
     if((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("Error: could not create socket.");
         return -1;
     }
+    else
+    {
+        printf("Socket created.");
+    }
+
+    serverName.sin_family = AF_INET;
 
     return 0;
 }
